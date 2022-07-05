a4951093ad6c104e22a9cf9626ca65b1687d24fd
JakeAve <jacobjavery@gmail.com>
2022-07-05 17:44:39 -0600

    fixed proxy server

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
 import { Login as LoginForm } from '../components/forms/Login'
 import { SignUp as SignupForm } from '../components/forms/SignUp'
 
