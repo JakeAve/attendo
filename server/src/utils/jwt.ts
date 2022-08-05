f06e1db514b574d6e539c350e11d659db3bf679a
JakeAve <jacobjavery@gmail.com>
2022-08-04 20:43:51 -0600

    starting refresh token work

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -4,15 +4,32 @@ import fs from 'fs'
 const privateKeyPath = './privateKey.json'
 const publicKeyPath = './publicKey.json'
 
+const privateKeyRefreshPath = './privateKeyRefresh.json'
+const publicKeyRefreshPath = './publicKeyRefresh.json'
+
 const rawPrivateKey = JSON.parse(fs.readFileSync(privateKeyPath, 'utf8'))
 const rawPublicKey = JSON.parse(fs.readFileSync(publicKeyPath, 'utf8'))
 
+const rawPrivateKeyRefresh = JSON.parse(
+  fs.readFileSync(privateKeyRefreshPath, 'utf8'),
+)
+const rawPublicKeyRefresh = JSON.parse(
+  fs.readFileSync(publicKeyRefreshPath, 'utf8'),
+)
+
 let privateKey: Uint8Array | jose.KeyLike
 let publicKey: Uint8Array | jose.KeyLike
+
+let privateKeyRefresh: Uint8Array | jose.KeyLike
+let publicKeyRefresh: Uint8Array | jose.KeyLike
 ;(async () => {
   try {
     privateKey = await jose.importJWK(rawPrivateKey)
     publicKey = await jose.importJWK(rawPublicKey)
+
+    privateKeyRefresh = await jose.importJWK(rawPrivateKeyRefresh)
+    publicKeyRefresh = await jose.importJWK(rawPublicKeyRefresh)
+
     console.log(`JWT keys ready`)
   } catch (err) {
     console.error(err)
@@ -34,8 +51,20 @@ export const signJwt = (payload: jose.JWTPayload) =>
     .setExpirationTime('30m')
     .sign(privateKey)
 
+export const signRefreshToken = (payload: jose.JWTPayload) =>
+  new jose.SignJWT(payload)
+    .setProtectedHeader({ alg: 'ES256' })
+    .setIssuedAt()
+    .setIssuer(issuer)
+    .setAudience(audience)
+    .setExpirationTime('24h')
+    .sign(privateKeyRefresh)
+
 export const verifyJwt = (jwt: string) =>
   jose.jwtVerify(jwt, publicKey, {
     issuer,
     audience,
   })
+
+export const verifyRefreshToken = (jwt: string) =>
+  jose.jwtVerify(jwt, publicKeyRefresh, { issuer, audience })
